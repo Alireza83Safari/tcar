97c97bb9e16a1246c170a268f6ea1689f233af35
Alireza <alireza83safarii@gmail.com>
2023-12-04 20:53:39 +0330

    feat: create contact page

---
+++ src/app/cantact/page.tsx
@@ -0,0 +1,15 @@
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+async function page() {
+  return (
+    <>
+      <Header />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default page;
