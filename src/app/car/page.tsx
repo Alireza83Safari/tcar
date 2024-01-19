f28db6eda90932bf8038a4957831ad685aff7223
Alireza <alireza83safarii@gmail.com>
2024-01-19 16:44:03 +0330

    feat: add totalCount

--- src/app/car/page.tsx
+++ src/app/car/page.tsx
@@ -1,44 +1,42 @@
 import Cars from "@/app/car/components/CarsContent";
+import Car from "@/models/car";
 import { apiUrl } from "@/services/apiUrl";
 
 export const dynamic = "force-dynamic";
 
 async function getCarss(searchParams: any) {
-  const { platform, color, company, carStatus, years, q } = searchParams;
+  const { platform, color, company, years, q, order, page } = searchParams;
+
   if (!apiUrl) {
-    return null;
+    return false;
   }
-  let APIURL = `car`;
-  if (platform) {
-    APIURL += `?platform=${platform}`;
-  }
-  if (color) {
-    APIURL += `?color=${color}`;
-  }
-  if (company) {
-    APIURL += `?company=${company}`;
-  }
-  if (carStatus) {
-    APIURL += `?carStatus=${carStatus}`;
-  }
-  if (years) {
-    APIURL += `?years=${years}`;
-  }
-  if (carStatus) {
-    APIURL += `?carStatus=${carStatus}`;
-  }
-  if (q) {
-    APIURL += `?q=${q}`;
-  }
+
+  let countQuery: any = {};
+
+  if (platform) countQuery.platform = platform;
+  if (color) countQuery.color = color;
+  if (company) countQuery.company = company;
+  if (years) countQuery.years = years;
+  if (q) countQuery.q = q;
+
+  const totalCount = await Car.countDocuments(countQuery);
+
+  let APIURL = `car?`;
+  if (platform) APIURL += `platform=${platform}&`;
+  if (color) APIURL += `color=${color}&`;
+  if (company) APIURL += `company=${company}&`;
+  if (years) APIURL += `years=${years}&`;
+  if (q) APIURL += `q=${q}&`;
+  if (order) APIURL += `order=${order}&`;
+  if (page) APIURL += `page=${page}&limit=6`;
 
   const res = await fetch(`${apiUrl}/api/${APIURL}`, { cache: "no-store" });
   const cars = await res.json();
 
-  return cars;
+  return { cars, total: totalCount };
 }
 
 export default async function page({ searchParams }: any) {
   const cars = await getCarss(searchParams);
-
-  return <Cars cars={cars} />;
+  return <Cars {...cars} />;
 }
