5a1d7536c04ae38a0407cc8c7fd4ab23c4818c54
Alireza <alireza83safarii@gmail.com>
2024-01-19 18:48:30 +0330

    feat: add next.config redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
